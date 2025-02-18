ea9c340c609a8f65f3ec62486147a609d1b6a0ea
Patrick Stünkel <webminz@googlemail.com>
2025-02-18 21:20:15 +0100

    implementing a lot more language features

--- edbs/EDBS.g4
+++ edbs/EDBS.g4
@@ -1,24 +1,62 @@
 grammar EDBS;
 import EDBSTokens;
 
-program :  statement* FINISH_KEYWORD NEWLINE?;
+program : module_def*  main_statement* FINISH_KEYWORD NEWLINE?;
 
-statement : ( write_stmt | calc_stmt | read_assgn) PERIOD NEWLINE;
+module_def : DEFINE_MOULE_KEYWORD IDENTIFIER MODULE_PARAM_KEYWORD NEWLINE? input_params NEWLINE? output_params NEWLINE? MODULE_BODY_KEYWORD COLON NEWLINE? module_body;
 
-read_assgn: READ_KEYWORD IDENTIFIER COLON STRING;
 
-write_stmt  : WRITE_KEYWORD write_arg+ ;
+input_params : INPUT_PARAM_KEYWORD COLON param_list;
+output_params :OUTPUT_PARAM_KEYWORD COLON IDENTIFIER;
+
+module_body : main_statement+;
+
+param_list : IDENTIFIER (COMMA IDENTIFIER)*;
+
+main_statement : stmt PERIOD NEWLINE;
+
+stmt : REPEAT_KEYWORD NEWLINE? stmt (COMMA NEWLINE? stmt)* NEWLINE? CONDITION_KEYWORD bool_expr # while
+    | READ_KEYWORD IDENTIFIER COLON STRING # read
+    | READ_KEYWORD FILE_KEYWORD IDENTIFIER # readfile
+    | WRITE_KEYWORD write_arg+  # write
+    | CALC_KEYWORD IDENTIFIER COLON expression # calc
+    | UPDATE_KEYWORD IDENTIFIER COLON expression # mutate
+    | EXIT_MODULE_KEYWORD # return
+    ;
+
 
 write_arg : STRING | IDENTIFIER;
 
-calc_stmt : CALC_KEYWORD IDENTIFIER COLON expression;
 
 expression:  expression OP_EXP expression # expo
         | expression OP_MUL expression # mul
+        | expression SOP_REPEAT expression # repeat
         | expression OP_DIV expression # div
+         | expression SOP_SPLIT expression # split
          | expression OP_ADD expression # add
+         | expression SOP_CONCAT expression # split
          | expression OP_SUB expression # sub
+         | expression SOP_SUBSTR expression # substr
          | '(' expression ')' # nested
-         | IDENTIFIER # var
-         |  NUMBER # lit
+         | CALL_MODULE_KEYWORD IDENTIFIER MODULE_PARAM_KEYWORD actual_param_list # call
+         | list_command IDENTIFIER (LOP_RESET_IDX expression)? # listop
+         | IDENTIFIER PRIMED? # var
+         | str_literal # strlit
+         |  NUMBER # nolit
         ;
+
+actual_param_list: actual_param (COMMA actual_param)*;
+
+actual_param: NUMBER | str_literal | IDENTIFIER;
+
+list_command : LOP_NEXT | LOP_BACK | LOP_RESET | LOP_FIND;
+
+str_literal : NULL_CHAR | NEWLINE_CHAR | WHITESPACE_CHAR | STRING;
+
+bool_expr : BOP_NOT bool_expr # not
+        | bool_expr BOP_AND bool_expr # and
+        | bool_expr BOP_OR bool_expr # or
+        | comparison # comp
+        ;
+
+comparison : expression (COMP_EQL | COMP_LT | COMP_LEQ | COMP_GT | COMP_GEQ) expression;
--- edbs/edbs/main.py
+++ edbs/edbs/main.py
@@ -1,4 +1,3 @@
-import sys
 from antlr4 import *
 from edbs.EDBSParser import EDBSParser
 from edbs.EDBSLexer import EDBSLexer
@@ -9,6 +8,7 @@ class SymbolTable:
 
     def __init__(self):
         self.storage = {}
+        self.modules = {}
 
     def add_var(self, name: str, value: float):
         self.storage[name] = value
@@ -18,39 +18,87 @@ class SymbolTable:
             raise KeyError(f"Variable {name} not found!!")
         return self.storage[name]
 
+    def is_defined(self, name: str):
+        return name in self.storage
+
+
+
 class InterpreterVisitor(EDBSVisitor):
 
     def __init__(self, symbol_table: SymbolTable):
         self.symbol_table = symbol_table
 
 
+    def visitModule_def(self, ctx:EDBSParser.Module_defContext):
+        name = str(ctx.IDENTIFIER())
+        self.visit(ctx.input_params())
+        self.symbol_table.modules[name] = ctx.module_body()
+
+    def visitInput_params(self, ctx:EDBSParser.Input_paramsContext):
+        self.visit(ctx.param_list())
+
+    def visitParam_list(self, ctx:EDBSParser.Param_listContext):
+        for c in ctx.children():
+            print(c)
+
     def visitWrite_arg(self, ctx:EDBSParser.Write_argContext):
         if ctx.IDENTIFIER() is not None:
             name = str(ctx.IDENTIFIER())
             value = self.symbol_table.get_var(name)
+            if isinstance(value, float):
+                value = str(round(value, 2)).replace(".", ",")
             print(str(value), end=" ")
         elif ctx.STRING():
             string_token = ctx.STRING()
             print(str(string_token)[1:-1], end=" ")
 
-    def visitWrite_stmt(self, ctx:EDBSParser.Write_stmtContext):
+    def visitWrite(self, ctx:EDBSParser.WriteContext):
         for c in ctx.children:
             self.visit(c)
         print()
 
-    def visitRead_assgn(self, ctx:EDBSParser.Read_assgnContext):
+    def visitRead(self, ctx:EDBSParser.ReadContext):
         prompt = str(ctx.STRING())[1:-1]
-        value = float(input(f"{prompt}: "))
+        value = float(input(f"{prompt}: ").replace(".", "").replace(",", "."))
         name = str(ctx.IDENTIFIER())
         self.symbol_table.add_var(name, value)
 
-    def visitCalc_stmt(self, ctx:EDBSParser.Calc_stmtContext):
+    def visitCalc(self, ctx:EDBSParser.CalcContext):
         name = str(ctx.IDENTIFIER())
+        value = self.visit(ctx.expression())
+        # if self.symbol_table.is_defined(name):
+        #     raise KeyError(f"Stedfortreter med namn «{name}» finnast allerede!")
+        self.symbol_table.add_var(name, value)
+
+    def visitMutate(self, ctx:EDBSParser.MutateContext):
+        name = str(ctx.IDENTIFIER())
+        if not self.symbol_table.is_defined(name):
+            self.symbol_table.add_var(name,0.0)
         value = self.visit(ctx.expression())
         self.symbol_table.add_var(name, value)
 
-    def visitLit(self, ctx:EDBSParser.LitContext):
-        value =  float(str(ctx.NUMBER()).replace(",", '.'))
+    def visitWhile(self, ctx:EDBSParser.WhileContext):
+        is_satisfied =  self.visit(ctx.bool_expr())
+        while is_satisfied:
+            for c in ctx.children:
+                self.visit(c)
+            is_satisfied = self.visit(ctx.bool_expr())
+
+    def visitStrlit(self, ctx:EDBSParser.StrlitContext):
+        return self.visit(ctx.str_literal())
+
+    def visitStr_literal(self, ctx:EDBSParser.Str_literalContext):
+        if ctx.STRING() is not None:
+            return str(ctx.STRING())[1:-1]
+        elif ctx.NEWLINE_CHAR():
+            return '\n'
+        elif ctx.WHITESPACE_CHAR():
+            return ' '
+        elif ctx.NULL_CHAR():
+            return None
+
+    def visitNolit(self, ctx:EDBSParser.NolitContext):
+        value = float(str(ctx.NUMBER()).replace(",", '.'))
         return value
 
     def visitVar(self, ctx:EDBSParser.VarContext):
@@ -76,10 +124,47 @@ class InterpreterVisitor(EDBSVisitor):
     def visitNested(self, ctx:EDBSParser.NestedContext):
         return self.visit(ctx.expression())
 
+    def visitListop(self, ctx:EDBSParser.ListopContext):
+        return None # TODO: hugseliste
+
+    def visitCall(self, ctx:EDBSParser.CallContext):
+        name = str(ctx.IDENTIFIER())
+        ctx.param_list()
+        exp = self.symbol_table.modules[name]
+        return None # TODO: parse
+
+    # bool expressions
+
+    def visitComp(self, ctx:EDBSParser.CompContext):
+        return self.visit(ctx.comparison())
+
+    def visitComparison(self, ctx:EDBSParser.ComparisonContext):
+        lhs = self.visit(ctx.getChild(0))
+        rhs = self.visit(ctx.getChild(ctx.getChildCount() - 1))
+        if ctx.COMP_EQL() is not None:
+            return lhs == rhs
+        elif ctx.COMP_LT() is not None:
+            return lhs < rhs
+        elif ctx.COMP_LEQ() is not None:
+            return lhs <= rhs
+        elif ctx.COMP_GT() is not None:
+            return lhs > rhs
+        elif ctx.COMP_GEQ() is not None:
+            return lhs >= rhs
+
+    def visitAnd(self, ctx:EDBSParser.AndContext):
+        return self.visit(ctx.getChild(0)) and self.visit(ctx.getChild(2))
+
+    def visitOr(self, ctx:EDBSParser.OrContext):
+        return self.visit(ctx.getChild(0)) or self.visitComp(ctx.getChild(2))
+
+    def visitNot(self, ctx:EDBSParser.NotContext):
+        return not self.visit(ctx.bool_expr())
+
 
 def main():
 
-    input_stream = FileStream("./examples/uttrykk.edbs", encoding="utf-8")
+    input_stream = FileStream("../examples/laan.edbs", encoding="utf-8")
     lexer = EDBSLexer(input_stream)
     token_stream = CommonTokenStream(lexer)
     parser = EDBSParser(token_stream)
